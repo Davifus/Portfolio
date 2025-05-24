b2845a342fdc0a0e5dc822b5431c57728c75d94f
David <gkim51@students.kennesaw.edu>
2025-05-24 05:05:09 -0400

    start of my react portfolio

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-red-500 text-white p-4">Hello</div>
+
+  
+  
+    </>
+  )
+}
+
+export default App
